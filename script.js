05a24460a2fbb67c632f60a056bbb9f19042e3af
Eduardo Felipe <156210174+Eduardo-Loff@users.noreply.github.com>
2025-03-22 21:22:03 -0300

    Remodelação do Projeto.
    Uma recriação do projeto, apenas por estudo.

--- script.js
+++ script.js
@@ -7,29 +7,29 @@ function calc(){
     res.innerHTML = ""
 
     res.innerHTML = `<h2>Exibindo Resultado</h2>`
-    res.innerHTML += `<p>x = -(${b}) ± √${b}² - 4 * ${a} * ${c}<br>----<br>2 * ${a}</p>`
+    res.innerHTML += `<p>x =  ( -(${b}) ± √${b}² - 4 * ${a} * ${c} ) / 2 * ${a} <br></p>`
 
     primeiro_calc = Number(-1 * b) // Calculo do Primeiro Elemento
     delta = Number(b* b - 4*a*c) // Calculo do Delta
     divisor = Number(2 * a) // Calculo do divisor
 
-    res.innerHTML += `<p>x = ${primeiro_calc} ± √${delta}<br>----<br>${divisor}</p>`
+    res.innerHTML += `<p>x = ( ${primeiro_calc} ± √${delta} ) / ${divisor} <br></p>`
 
     if(delta >= 0){
         raiz = Math.sqrt(delta) // Tira a Raiz
         if(Number(raiz - parseInt(raiz)) == 0){
-            res.innerHTML += `<p> x = ${primeiro_calc} ± ${raiz}<br>-----<br>${divisor}</p>`
+            res.innerHTML += `<p> x = ( ${primeiro_calc} ± ${raiz} ) / ${divisor} <br></p>`
             x_1 = primeiro_calc + raiz
             x11 = x_1 / divisor
-            res.innerHTML += `<p>x' = ${primeiro_calc} + ${raiz}<br>-----<br>${divisor} </p>`
-            res.innerHTML += `<p>x' = ${x_1}<br>----<br>${divisor} </p>`
-            res.innerHTML += `<p>x' = ${x11}`
+            res.innerHTML += `<p>x' = ( ${primeiro_calc} + ${raiz} ) / ${divisor} <br></p>`
+            res.innerHTML += `<p>x' = '${x_1}  / ${divisor} <br></p>`
+            res.innerHTML += `<p>x' = ${x11}<br></p>`
 
             x_2 = primeiro_calc - raiz
             x22 = x_2 / divisor
-            res.innerHTML += `<p>x'' = ${primeiro_calc} - ${raiz}<br>-----<br>${divisor} </p>`
-            res.innerHTML += `<p>x'' = ${x_2}<br>----<br>${divisor} </p>`
-            res.innerHTML += `<p>x'' = ${x22}`
+            res.innerHTML += `<p>x'' =  ( ${primeiro_calc} + ${raiz} ) / ${divisor} <br></p>`
+            res.innerHTML += `<p>x'' = ${x_2} / ${divisor} <br></p>`
+            res.innerHTML += `<p>x'' = ${x22}<br></p>`
         
         }
         else{
